425d2a46636be631d3a6d6709335d3424268a0fb
liliwang1 <monsi0510@hotmail.com>
2020-08-13 00:41:06 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,12 @@
 // helloWorld function 
 function helloWorld() {
     return "Hello, World!";
+    // console.log("Hello World!");
+}
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
+
+function sayHello(name) {
+    return "hello" + name;
 }
